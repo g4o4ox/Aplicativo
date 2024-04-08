28d139c80f51a4957532d494e17e69202e380c9c
g4o4x <brasilrotas116@gmail.com>
2024-04-08 17:59:55 -0300

    Update Color and Navigation system

--- app/src/main/java/com/example/myapplication/view/MainScreen.kt
+++ app/src/main/java/com/example/myapplication/view/MainScreen.kt
@@ -6,7 +6,9 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.materialIcon
 import androidx.compose.material3.Button
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -19,14 +21,15 @@ import androidx.navigation.NavController
 fun MainScreen(navController: NavController){
     //Isac Motta --> Criando o menu de botões parte Inferior.
    Box(modifier= Modifier.fillMaxSize()){
-       Row(modifier = Modifier .fillMaxWidth() .align(Alignment.BottomCenter) .background(color = Color.Gray)){
+       Row(modifier = Modifier
+           .fillMaxWidth()
+           .align(Alignment.BottomCenter)
+           .background(color = Color.Gray)){
            TextButton(onClick = { navController.navigate(route = "RollGame") },modifier = Modifier ) {
                Text("jogar")
            }
-           TextButton(onClick = { navController.navigate(route = "UserPerfil") }){
-               Text("Perfil")
+
            }
        }//Fechamento do Row
    } //fechamento do Box <--- Isac ---->
 
-}
